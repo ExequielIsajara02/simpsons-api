ca65a4c34c063dfb3f8fa92f3b43b113c122b1f7
ExequielIsajara02 <exequielisajara@gmail.com>
2024-11-08 00:01:11 -0300

    layers created

---
+++ src/test/java/com/simpsons_api/SimpsonsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simpsons_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpsonsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
